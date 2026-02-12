fb0eb266c18d93c58bec77fb203302e2c28149b9
Lingeshwaran <lingeshwaran.kv2022cse@sece.ac.in>
2026-02-12 21:33:18 +0530

    added the project to the repo

---
+++ control_plane/src/main/java/net/vuega/control_plane/ControlPlaneApplication.java
@@ -0,0 +1,13 @@
+package net.vuega.control_plane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlPlaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlPlaneApplication.class, args);
+	}
+
+}
